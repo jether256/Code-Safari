6f8b23cd41b74969f0c77b93e79ff34b509b9d99
mutale jether <mutalejether>
2023-07-14 17:22:52 +0300

    'first'

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Code Safari</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+
+	<nav>
+		<div class="nav-left">
+			<img src="assets/images/logo.png" alt="Code Safari">
+
+			<ul>
+				<li><img src="assets/images/home.png" ></li>
+				<li><img src="assets/images/live.png" ></li>
+				<li><img src="assets/images/notification.png" ></li>
+				<li><img src="assets/images/video.png" ></li>
+			</ul>
+		</div>
+
+		<div class="nav-right">
+
+			<div class="search-box">
+
+				<img src="assets/images/search.png" >
+				<input type="text" name="" placeholder="Search">
+			</div>
+
+			<div class="nav-user-icon online">
+				<img src="assets/images/member-1.png" >
+			</div>
+			
+		</div>
+	</nav>
+
+
+	<!-- left right center containers -->
+	<div class="container">
+
+
+		<!-- left side bar start -->
+		<div class="left-side-bar">
+			
+			<div class="imp-links">
+				<a href="#"><img src="assets/images/member-1.png">Mutale Jether <br> PHP Developer</a>
+				<a href="#"><img src="assets/images/member-2.png">Rodney Musinguzi <br> PHP Developer</a>
+				<a href="#"><img src="assets/images/member-3.png">Lukyamuzi Alex <br> PHP Developer</a>
+				<a href="#"><img src="assets/images/member-4.png">Lutaya <br> PHP Developer</a>
+				<a href="#"><img src="assets/images/member-7.png">Zadoki <br> PHP Developer</a>
+				<a href="#">See more..</a>
+			</div>
+
+
+			<div class="shortcut-links">
+				<p>Your Shortcuts</p>
+				<a href="#"><img src="assets/images/logo.png">Web Developers</a>
+				<a href="#"><img src="assets/images/logo.png">Web Design course</a>
+				<a href="#"><img src="assets/images/logo.png">Full Stack Development</a>
+				<a href="#"><img src="assets/images/logo.png">Web Experts</a>
+			</div>
+
+		</div>
+		<!-- left-sidebar-end -->
+
+
+
+
+
+		<!-- main bar start -->
+		<div class="main-bar"></div>
+		<!-- main bar end -->
+
+
+
+		<!-- right-sidebar start -->
+		<div class="right-side-bar"></div>
+		<!-- right-side-bar end -->
+		
+
+
+
+	</div>
+
+</body>
+</html>
